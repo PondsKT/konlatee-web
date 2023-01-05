70ab4eb2b097c8de42577f57b75336b73c874986
Konlatee <konlatee@MacBook-Air-Konlatee.local>
2023-01-06 03:06:19 +0700

    feat: init commit

---
+++ components/MyProfile.vue
@@ -0,0 +1,16 @@
+<template>
+  <main class="flex bg-gray-200 h-screen p-5 justify-center">
+    <div class="profile shadow p-5 m-auto rounded-lg">MyProfile {{ name }}</div>
+  </main>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const name = ref('konlatee')
+</script>
+
+<style>
+.profile {
+  background-color: white;
+}
+</style>
